5d936b489a8e1ed9554da5bb5fd5149aecf664f0
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-05-17 13:36:20 +0200

    Prepare for user project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,19 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform) 
+    alias(libs.plugins.kotlinMultiplatform)
 }
 
 group = "me.user"
 version = "1.0-SNAPSHOT"
 
+val kotlin_repo_url: String? = project.properties["kotlin_repo_url"] as String?
+val language_version: String? = project.properties["language_version"] as String?
+
 repositories {
     mavenCentral()
+
+    kotlin_repo_url?.also { maven(it) }
 }
 
 kotlin {
@@ -22,6 +27,14 @@ kotlin {
             commonWebpackConfig {
                 devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
                     static(projectDirPath)
+                }
+            }
+        }
+
+        compilations.configureEach {
+            compileTaskProvider.configure {
+                language_version?.let {
+                    compilerOptions.languageVersion.set(fromVersion(it))
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,25 @@
 rootProject.name = "kotlin-wasm-browser-template"
 
 pluginManagement {
+    val kotlin_repo_url: String? by settings
+
     resolutionStrategy {
         repositories {
             gradlePluginPortal()
+
+            kotlin_repo_url?.also { maven(it) }
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    val kotlin_version: String? by settings
+
+    versionCatalogs {
+        create("libs") {
+            kotlin_version?.let {
+                version("kotlin", it)
+            }
         }
     }
 }
