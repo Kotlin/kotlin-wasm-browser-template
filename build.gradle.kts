285e306e79923db9043da67ebdf21c30322fce91
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-08-05 12:46:15 +0200

    Upgrade Kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    kotlin("multiplatform") version "1.9.22"
+    kotlin("multiplatform") version "2.0.0"
 }
 
 repositories {
