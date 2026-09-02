0911c37ed04f711f9175437d513654fd6470e218
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2026-09-02 15:20:20 +0200

    Fix compilation of build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform) 
@@ -20,9 +21,7 @@ kotlin {
             val projectDirPath = project.projectDir.path
             commonWebpackConfig {
                 devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        add(projectDirPath)
-                    }
+                    static(projectDirPath)
                 }
             }
         }
